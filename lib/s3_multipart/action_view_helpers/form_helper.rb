71da45c621aceafce8d7659232e01abf8e28654c
Arnar Tumi Þorsteinsson <arnartumi@gmail.com>
2013-11-11 15:51:59 +0000

    wip

--- lib/s3_multipart/action_view_helpers/form_helper.rb
+++ lib/s3_multipart/action_view_helpers/form_helper.rb
@@ -4,12 +4,7 @@ module S3Multipart
       def multipart_uploader_form(options = {})
         uploader = S3Multipart::Uploader
         uploader_digest = uploader.serialize(options[:uploader])
-
-        html = file_field_tag options[:input_name], :multiple => 'multiple', :data => {:uploader => uploader_digest}
-        html << options[:html].html_safe
-        html << button_tag(:class => options[:button_class]) do
-          content_tag(:span, options[:button_text])
-        end
+        html = file_field_tag options[:input_name], :multiple => 'multiple', :onchange => 'javascript:this.form.submit();' :data => {:uploader => uploader_digest}
         html << '<ul class="upload-container"></ul>'.html_safe
       end
     end
